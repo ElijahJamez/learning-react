742b53c3a2f0158940d4b24e05aa19c10e26c9f2
Elijah Barnett <elijahbarnett@Elijahs-MacBook-Pro.local>
2023-11-14 14:57:32 -0600

    step 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-
-
+import { useState } from 'react'                                   //returns an array of all the puppies
+import { puppyList } from './data.js'                              //imports the array into the App.jsx component
 import './App.css'
+import React from 'react'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-    
+  const [puppies, setPuppies] = useState(puppyList)               //"[Value that you are storing, Function used to reset the value]" - You can deconstruct both of those values from the array
+  console.log(puppies)  
+    return (
+      <div className ="App">
+        { 
+         puppies.map((puppy) => {
+          return <p key ={puppy.id}>{puppy.name}</p>
+          })  
+        }
       </div>
-      
-    </>
-  )
+
+    )
 }
 
 export default App
